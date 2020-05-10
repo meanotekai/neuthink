9a8e9d6d0b2842e93689b998321e630df69e4ee5
denis <dtarasov3@gmail.com>
2020-05-10 15:17:03 +0300

    improvements in usplit, and support for encoding in load_lines function

--- functional.py
+++ functional.py
@@ -1,16 +1,30 @@
+from typing import List
+
 def load_file(name, encoding='utf-8'):
     '''just load text file'''
     return open(name,errors="ignore",encoding=encoding).read()
 
-def load_lines(name):
+def save_file(name, string):
+    '''just save text file'''
+    return open(name, "w", errors="ignore",encoding='utf-8').write(string)
+
+def load_lines(name:str) -> List[str]:
     '''just load lines from text file'''
     f = open(name)
     lines = f.readlines()
     f.close()
     return [x.strip() for x in lines]
 
-def usplit(text, separators=[], keep_separators=True, keep_space=False, return_positions=True):
-    '''The ultimate split function. Alternative to split. 
+def save_lines(name:str, lines:List[str]):
+    '''just saves lines to text file'''
+    f = open(name,"w",encoding='utf-8')
+    for x in lines:
+      f.write(x+'\n')
+    f.close()
+
+
+def usplit(text, separators=[], keep_separators=True, keep_space=False, keep_new_line=False,  return_positions=True):
+    '''The ultimate split function. Alternative to split.
        Args:
          text: text to split
          separators: list of separators, default is all punctionation and space
@@ -25,9 +39,12 @@ def usplit(text, separators=[], keep_separators=True, keep_space=False, return_p
              if return_positions:
                 buffer.append((word_buffer,i-len(word_buffer),i))
              else:
-                buffer.append(word_buffer) 
-        return  (''  if ((x==" ") or x=="\n") and (not keep_space) else x)
-    
+                buffer.append(word_buffer)
+        if keep_new_line:
+            return  (''  if ((x==" ")) and (not keep_space) else x)
+        else:
+            return  (''  if ((x==" ") or x=="\n") and (not keep_space) else x)
+
     if separators==[]:
         separators = set([u";", u",", u".", u"(", u")", u"!", u"?", u":", u"/",
     u"\r", u"\n", u"\t", u"*", u"~", u"+", u"=", u'"', u" ", u" ",
@@ -51,7 +68,9 @@ def usplit(text, separators=[], keep_separators=True, keep_space=False, return_p
                 state=0
 
             word_buffer = word_buffer + x
-    buffer.append((word_buffer,len(text)-len(word_buffer),len(text)))    
-    return buffer
+    if return_positions:
+        buffer.append((word_buffer,len(text)-len(word_buffer),len(text)))
+    else:
+        buffer.append(word_buffer)
 
-    
+    return buffer
