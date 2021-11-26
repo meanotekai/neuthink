5eea97ee5bff10b1d716a1b0df9b1c27745c4506
Durham <dtarasov3@gmail.com>
2021-11-26 15:27:39 +0300

    bert features

---
+++ extapi/bert/bert_features.py
@@ -0,0 +1,22 @@
+from typing import List
+import sys
+import numpy as np
+import torch
+from transformers import BertTokenizer, BertForSequenceClassification
+from transformers import logging
+
+logging.set_verbosity_error()
+
+# this part of the code creates BERT model
+model = BertForSequenceClassification.from_pretrained('bert-base-multilingual-uncased')
+tokenizer = BertTokenizer.from_pretrained('bert-base-multilingual-uncased')
+labels = torch.tensor([1]).unsqueeze(0)
+
+
+def bert_vector(text:str):
+   '''computes vector representation of text using BERT'''
+   text = text[0:300]
+   inputs = tokenizer(text.lower(), return_tensors="pt")
+   outputs = model(**inputs, labels=labels, output_hidden_states=True)
+   q = outputs[2][11][0]
+   return q.mean(dim=0).cpu().detach().numpy()
